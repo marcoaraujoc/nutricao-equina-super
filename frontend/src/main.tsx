f5b28a3e4c1926144ba49f428601aeb6fb5b6f38
marcoaraujoc <101113539+marcoaraujoc@users.noreply.github.com>
2026-04-22 12:26:36 +0000

    Versao Estavel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,3 @@ import App from './App.tsx'
 import './index.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const GOOGLE_CLIENT_ID = '1032724797526-vji8mbs4fsjggm0odh8kttudroekeae5.apps.googleusercontent.com'   // ← troque aqui
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>,
-)
-
-// Chave Secreta - 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
